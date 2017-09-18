a235f05c61eec560b73400b65985f5e086870606
Sam Cordingley <samcordingley@gmail.com>
2017-09-18 16:16:14 +0100

    update

--- script.js
+++ script.js
@@ -29,6 +29,111 @@ $( '#pane-heatmap').click(function() {
   $('#home').addClass("top");
 });
 
+$( '#pane-lightfield').click(function() {
+  $('#img-lightfield').removeClass("top");
+  $('#img-lightfield').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#lightfield').removeClass("bottom");
+  $('#lightfield').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+$( '#pane-tombofterror').click(function() {
+  $('#img-tombofterror').removeClass("top");
+  $('#img-tombofterror').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#tombofterror').removeClass("bottom");
+  $('#tombofterror').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+$( '#pane-rasteriser').click(function() {
+  $('#img-rasteriser').removeClass("top");
+  $('#img-rasteriser').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#rasteriser').removeClass("bottom");
+  $('#rasteriser').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+
+$( '#pane-raytracer').click(function() {
+  $('#img-raytracer').removeClass("top");
+  $('#img-raytracer').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#raytracer').removeClass("bottom");
+  $('#raytracer').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+
+$( '#pane-robot').click(function() {
+  $('#img-robot').removeClass("top");
+  $('#img-robot').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#robot').removeClass("bottom");
+  $('#robot').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+$( '#pane-animation').click(function() {
+  $('#img-animation').removeClass("top");
+  $('#img-animation').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#animation').removeClass("bottom");
+  $('#animation').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+$( '#pane-freezebox').click(function() {
+  $('#img-freezebox').removeClass("top");
+  $('#img-freezebox').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#freezebox').removeClass("bottom");
+  $('#freezebox').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+
+
+$( '#pane-geddit').click(function() {
+  $('#img-geddit').removeClass("top");
+  $('#img-geddit').addClass("current");
+  $('#projects').removeClass("current");
+  $('#projects').addClass("bottom");
+
+  $('#geddit').removeClass("bottom");
+  $('#geddit').addClass("current");
+  $('#home').removeClass("current");
+  $('#home').addClass("top");
+});
+
+
+
+
+
+
 $( '#pane-myenv').click(function() {
   $('#img-myenvironment').removeClass("top");
   $('#img-myenvironment').addClass("current");
